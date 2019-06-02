e2305fd1fbed1c2bdc299aa3c840a5deb4eee007
Matt <matthew.endicott46@gmail.com>
2019-06-01 22:04:26 -0600

    added button to toggle button text color // added button to toggle button text color // added button to strip all CSS for a timed period // added button to apply a waving rainbow rgba hover effect

--- javascript/main.js
+++ javascript/main.js
@@ -7,11 +7,50 @@ function backgronudImageChange() {
   const newImage = 'url(../images/getRolled.png) no-repeat center center fixed';
   const imageDefaults = document.body.style;
 
-  console.log('Backgroud-Image Change Applied');
-
   setTimeout(() => {
     document.body.style = imageDefaults;
   }, 4000);
 
   imageDefaults.background = newImage;
 }
+
+// ? Color Change
+function changeTextColor() {
+  const neonShadow =
+    '0 0 10px #fff, 0 0 20px #fff, 0 0 30px #e60073, 0 0 40px #e60073, 0 0 50px #e60073, 0 0 60px #e60073, 0 0 70px #e60073';
+  let textTags = document.getElementsByClassName('card');
+
+  for (i = 0; i < textTags.length; ++i) {
+    if (textTags[i].style.textShadow == 'none') {
+      textTags[i].style.textShadow = neonShadow;
+    } else {
+      textTags[i].style.textShadow = 'none';
+    }
+  }
+}
+
+// ? Remove All CSS
+function removeCSS() {  
+    setTimeout(() => {
+        document.styleSheets[0].disabled = false;
+    }, 4000);
+  
+    document.styleSheets[0].disabled = true;
+  }
+
+// ? Better Hover Effects
+// TODO finish this my dude
+function betterHover() {
+    let textTags = document.getElementsByClassName('card');
+    
+    console.log(textTags[1].classList);
+    for (i = 0; i < textTags.length; ++i) {
+        textTags[i].classList.toggle('betterHover');
+    }
+}
+
+// ! API Buttons
+
+// ? Weather in Antarica
+// function getWeather() {
+// }
